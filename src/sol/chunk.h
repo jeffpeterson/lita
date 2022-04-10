9e48ef8934f42d284d4236e4dfa9652fbdf0e761
Jeff Peterson <jeff@yak.sh>
2022-04-09 17:42:31 -0700

    Label stack operations

--- src/sol/chunk.h
+++ src/sol/chunk.h
@@ -5,63 +5,63 @@
 #include "value.h"
 
 typedef enum OpCode {
-  OP_CONSTANT,
+  OP_CONSTANT,      /** (n) [] -> [0 x] */
+                    /**/
+  OP_NIL,           /** [] -> [0 nil] */
+  OP_TRUE,          /** [] -> [0 true] */
+  OP_FALSE,         /** [] -> [0 false] */
+                    /**/
+  OP_RANGE,         /** [1 start][0 end] -> [0 Range] */
+  OP_TUPLE,         /** (n) [n ...args][0 arg] -> [0 tuple] */
+                    /**/
+  OP_PEEK,          /** (n) [...] -> [...][0 peek(n)] */
+  OP_POP,           /** [0 x] -> [] */
+  OP_POPN,          /** [n x][...] -> [] */
+  OP_SWAP,          /** (a, b) [a x][b y] -> [a y][b x] */
+  OP_DEFAULT,       /** (x) [] -> [0 x] */
+                    /**/
+  OP_DEFINE_GLOBAL, /** (name) [0 value] -> [] */
+  OP_GET_GLOBAL,    /** (name) [] -> [0 value] */
+  OP_SET_GLOBAL,    /** (name) [0 value] -> [0 value] */
+                    /**/
+  OP_GET_LOCAL,     /** (slot) [] -> [0 value] */
+  OP_SET_LOCAL,     /** (slot) [0 value] -> [0 value] */
+                    /**/
+  OP_GET_PROPERTY,  /** (name) [0 self] -> [0 value] */
+  OP_SET_PROPERTY,  /** (name) [1 self][0 value] -> [0 value] */
+                    /**/
+  OP_GET_UPVALUE,   /** (n) [] -> [0 value] */
+  OP_SET_UPVALUE,   /** (n) [0 value] -> [0 value] */
+                    /**/
+  OP_GET_SUPER,     /** (name) [0 super] -> [0 method] */
+                    /**/
+  OP_EQUAL,         /** [1 a][0 b] -> [0 Bool] */
+  OP_GREATER,       /** [1 a][0 b] -> [0 Bool] */
+  OP_LESS,          /** [1 a][0 b] -> [0 Bool] */
+                    /**/
+  OP_ADD,           /** [1 a][0 b] -> [0 a+b] */
+  OP_SUBTRACT,      /** [1 a][0 b] -> [0 a-b] */
+  OP_MULTIPLY,      /** [1 a][0 b] -> [0 a*b] */
+  OP_DIVIDE,        /** [1 a][0 b] -> [0 a/b] */
+                    /**/
+  OP_NOT,           /** [0 x] -> [0 !x] */
+  OP_NEGATE,        /** [0 x] -> [0 -x] */
+                    /**/
+  OP_JUMP,          /** (2n) [] -> [] */
+  OP_JUMP_IF_FALSE, /** (2n) [0 x] -> [0 x] */
+  OP_LOOP,          /** (2n) [] -> [] */
 
-  OP_NIL,
-  OP_TRUE,
-  OP_FALSE,
-
-  OP_RANGE,
-  OP_TUPLE,
-
-  OP_PEEK,
-  OP_POP,
-  OP_POPN,
-  OP_SWAP,
-  OP_DEFAULT,
-
-  OP_DEFINE_GLOBAL,
-  OP_GET_GLOBAL,
-  OP_SET_GLOBAL,
-
-  OP_GET_LOCAL,
-  OP_SET_LOCAL,
-
-  OP_GET_PROPERTY,
-  OP_SET_PROPERTY,
-
-  OP_GET_UPVALUE,
-  OP_SET_UPVALUE,
-
-  OP_GET_SUPER,
-
-  OP_EQUAL,
-  OP_GREATER,
-  OP_LESS,
-
-  OP_ADD,
-  OP_SUBTRACT,
-  OP_MULTIPLY,
-  OP_DIVIDE,
-
-  OP_NOT,
-  OP_NEGATE,
-
-  OP_JUMP,
-  OP_JUMP_IF_FALSE,
-  OP_LOOP,
-
-  OP_CALL,
-  OP_CLASS,
-  OP_INHERIT,
-  OP_METHOD,
-  OP_CLOSURE,
-  OP_CLOSE_UPVALUE,
-  OP_PRINT,
-  OP_RETURN,
-
-  OP_INVOKE,
-  OP_SUPER_INVOKE,
+  OP_CALL,          /** (argc) [] -> [] */
+  OP_CLASS,         /** (name) [] -> [0 class] */
+  OP_INHERIT,       /** [1 class][0 super] */
+  OP_METHOD,        /** [1 class][0 closure] -> [0 class] */
+  OP_CLOSURE,       /** (fn) [] -> [0 closure] */
+  OP_CLOSE_UPVALUE, /** [0 upvalue] -> [] */
+  OP_PRINT,         /** [0 x] -> [] */
+  OP_RETURN,        /** [0 x] -> [] */
+                    /**/
+  OP_INVOKE,        /** (name, n) [n self][0 ...args] -> [] */
+  OP_SUPER_INVOKE,  /** (name, n) [n+1 self][1 ...args][0 super] -> [] */
 } OpCode;
 
 typedef struct {
