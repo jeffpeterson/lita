c524a094191dad36feb2fdc299c525a1f8d6e689
Jeff Peterson <jeff@yak.sh>
2023-01-28 11:47:12 -0800

    graphs?

--- src/lita/graph.c
+++ src/lita/graph.c
@@ -3,15 +3,11 @@
 #include "graph.h"
 
 void init_graph(Graph *graph) {
-  // DB *db;
-  // int err = db_create(&db, NULL, 0);
-
-  // if (err) {
-  //   printf("db_create error: %i", err);
-  //   exit(1);
-  // }
-
-  //   db->open(db, NULL, )
+  initTable(&graph->nodes);
+  initTable(&graph->edges);
 }
 
-void free_graph(Graph *graph) {}
+void free_graph(Graph *graph) {
+  freeTable(&graph->nodes);
+  freeTable(&graph->edges);
+}
--- src/lita/graph.h
+++ src/lita/graph.h
@@ -1,22 +1,20 @@
 #ifndef lita_graph_h
 #define lita_graph_h
 
-// #include <db.h>
-// #include <ossp/uuid.h>
-
 #include "common.h"
-
-// #include "store.h"
-
-// typedef uuid_t Id;
+#include "object.h"
+#include "table.h"
 
 typedef struct Graph {
-  // DB db;
-  // Store links;
-  // Store data;
+  Table nodes;
+  Table edges;
 } Graph;
 
 void init_graph(Graph *graph);
 void free_graph(Graph *graph);
+
+void graph_edge(ObjTuple *edge);
+void graph_add(Value index);
+void graph_disconnect(ObjTuple *edge);
 
 #endif
