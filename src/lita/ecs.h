f5b0a23c9f6dd7f35db54e4ec6f86ae5973196a6
Jeff Peterson <jeff@yak.sh>
2024-09-02 20:36:32 -0700

    add some commented flecs options

--- src/lita/ecs.h
+++ src/lita/ecs.h
@@ -1,6 +1,19 @@
 #ifndef lita_ecs_h
 #define lita_ecs_h
 
+// https://github.com/SanderMertens/flecs/blob/master/docs/Quickstart.md#addons
+
+// #define FLECS_CUSTOM_BUILD
+
+// #define FLECS_HTTP
+// #define FLECS_MODULE
+// #define FLECS_SYSTEM
+// #define FLECS_PIPELINE
+// #define FLECS_TIMER
+// #define FLECS_JSON
+// #define FLECS_REST
+// #define FLECS_APP
+
 #include "flecs.h"
 
 typedef ecs_world_t World;
